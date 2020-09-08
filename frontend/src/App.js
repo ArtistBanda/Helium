21ce5c087c41330a18fb519c5dacfa7b65eadbdf
ArpitSharma2800 <51937952+ArpitSharma2800@users.noreply.github.com>
2020-09-08 10:10:33 +0530

    setup of firebase

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import fire from "./fire";
 
 function App() {
+  useEffect(() => {
+    let mes = fire
+      .database()
+      .ref("products")
+      .child("9")
+      .child("name")
+      .on("value", (snap) => {
+        console.log(snap.val());
+      });
+
+    let meds = fire
+      .firestore()
+      .collection("registration")
+      .get()
+      .then((querySnapshot) => {
+        const tempDoc = querySnapshot.docs.map((doc) => {
+          return { id: doc.id, ...doc.data() };
+        });
+        console.log(tempDoc);
+      });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +32,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
