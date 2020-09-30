18f2f02623a525f06f1011f73ca51b0b9d53b816
ArtistBanda <chirag.codes@gmail.com>
2020-10-01 00:18:41 +0530

    Removed cred

--- frontend/src/fire.js
+++ frontend/src/fire.js
@@ -1,14 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCbfTNeB9bP_rwXN1lq3_4AGkqNTxQD6uQ",
-  authDomain: "functions-801a7.firebaseapp.com",
-  databaseURL: "https://functions-801a7.firebaseio.com",
-  projectId: "functions-801a7",
-  storageBucket: "functions-801a7.appspot.com",
-  messagingSenderId: "737546208791",
-  appId: "1:737546208791:web:8f3744468857c29fbafe52",
-  measurementId: "G-GDTPPSDM72",
+  'KEY'
 };
 
 var fire = firebase.initializeApp(firebaseConfig);
